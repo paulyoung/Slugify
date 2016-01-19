38bc9cae613200dcb1c80e7c81939bae0a7832b7
Paul Young <paulyoungonline@gmail.com>
2016-01-19 15:01:35 -0800

    Add inital Xcode project.

---
+++ Slugify/Slugify.h
@@ -0,0 +1,19 @@
+//
+//  Slugify.h
+//  Slugify
+//
+//  Created by Paul Young on 1/19/16.
+//  Copyright © 2016 Paul Young. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Slugify.
+FOUNDATION_EXPORT double SlugifyVersionNumber;
+
+//! Project version string for Slugify.
+FOUNDATION_EXPORT const unsigned char SlugifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slugify/PublicHeader.h>
+
+
